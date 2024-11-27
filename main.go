ea58f2f9aba05d33608cf5ebe7149e54035a4bbf
haeyeon, hwang <haeyun@gmail.com>
2024-11-27 10:07:16 +0900

    make it better

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-"fmt"
-"log"
-"net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +18,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
  \    \         __/
   \____\_______/
 
+  
 	
 Hello from Docker!
 
@@ -28,4 +29,3 @@ func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
